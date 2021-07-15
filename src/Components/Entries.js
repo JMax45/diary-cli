c248ecaeeee6efc2a48adadeb6bcd639f3cd8d93
JMax45 <maxmailru.45@gmail.com>
2021-07-15 15:34:44 +0200

    feat: add home shortcut in entries

--- src/Components/Entries.js
+++ src/Components/Entries.js
@@ -3,6 +3,8 @@ import React, { useContext, useState } from "react";
 import { PageContext } from "./PageContext";
 import { StorageContext } from "../Storage";
 import SingleEntry from "./SingleEntry";
+import { Box, Newline, Text, useInput } from "ink";
+import Main from './Main';
 
 const Entries = () => {
     const [page, setPage] = useContext(PageContext).page;
@@ -11,8 +13,24 @@ const Entries = () => {
         label: `${e.date} | ${e.text.substring(0, 30)}...`, 
         value: e.id 
     })));
-
-    return <SelectInput items={items} onSelect={(item) => { setPage(<SingleEntry entryId={item.value} />) }} />;
+    useInput((input, key) => {
+        if(!key.ctrl) {
+            return;
+        }
+        switch(input.toLowerCase()) {
+            case 'q':
+                setPage(<Main />);
+                break;
+        }
+	});
+    return <>
+        <SelectInput items={items} onSelect={(item) => { setPage(<SingleEntry entryId={item.value} />) }} />
+        <Newline />
+        <Box>
+            <Text backgroundColor="white" color="black">^Q</Text>
+            <Text> Home</Text>
+        </Box>
+    </>
 }
 
 export default Entries;
